397cc6ae7157c7c9011f20bc98885166f444a51a
Tim Liu <liu33173847@hotmail.com>
2013-05-14 00:32:12 +1000

    using new plugin for sending email

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,9 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-     config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
+
+    config.action_mailer.delivery_method   = :postmark
+    config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
   end
 end
